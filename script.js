cbeb5eb80ef0675629f4690c5eca738159ff267e
pocha <pocha@stalkninja.com>
2013-04-03 17:25:08 +0530

    appended all ids & classes with 'shah-' so that the css can be included in other pages as well

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-	$("#nav li").click(function(){
-		$(".arrow-down").css({'display':'none'});
-		$("#nav li").removeClass("active-nav");
+	$("#shah-nav li").click(function(){
+		$(".shah-arrow-down").css({'display':'none'});
+		$("#shah-nav li").removeClass("active-nav");
 		//$("#preview-image").attr('src',"images/preview-images/"+this.id.replace("nav-","")+".png");
-		$("#preview-image").attr('src',$("#preview-image").attr("src").replace(/[a-z]+\.png$/,this.id.replace("nav-","")+".png"));
+		$("#shah-preview-image").attr('src',$("#shah-preview-image").attr("src").replace(/[a-z]+\.png$/,this.id.replace("shah-nav-","")+".png"));
 		$(this).addClass("active-nav");
 		console.log("#"+this.id+" .arrow-down");
 		$("#"+this.id+" .arrow-down").css({'display':'block'});
