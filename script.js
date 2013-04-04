46aec0ea03802e194a416e122fdcf3c21ccc9e3a
pocha <pocha@stalkninja.com>
2013-04-04 16:29:22 +0530

    changed switching of images so that it works for production server

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 	$("#shah-nav li").click(function(){
 		$(".shah-arrow-down").css({'display':'none'});
-		$("#shah-nav li").removeClass("active-nav");
+		$("#shah-nav li").removeClass("shah-active-nav");
 		//$("#preview-image").attr('src',"images/preview-images/"+this.id.replace("nav-","")+".png");
-		$("#shah-preview-image").attr('src',$("#shah-preview-image").attr("src").replace(/[a-z]+\.png$/,this.id.replace("shah-nav-","")+".png"));
-		$(this).addClass("active-nav");
+		$("#shah-preview-image").attr('src', $("#shah-preload-images img")[this.id.replace(/shah-image-/,"")].src );
+		$(this).addClass("shah-active-nav");
 		console.log("#"+this.id+" .arrow-down");
 		$("#"+this.id+" .arrow-down").css({'display':'block'});
 	});
